7fdebaf95634af3b486bf483f35076f34ccef063
natanael1289 <natanaelcab1289@gmail.com>
2026-03-16 22:07:10 -0300

    creacion

---
+++ AplicacionClase1/Estudiante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionClase1
+{
+    public class Estudiante
+    {
+        private int aId;
+        private string aNombre;
+        private int aEdad;
+        private double aNota;
+
+        public int Id { get { return aId; } set { aId = value; } }
+        public string Nombre { get { return aNombre; } set { aNombre = value; } }
+        public int Edad { get { return aEdad; } set { aEdad = value; } }
+        public double Nota { get { return aNota; } set { aNota = value; } }
+
+        public override string ToString()
+        {
+            return "Mi nombre es: " + this.Nombre + " Mi edad es: " + this.Edad + " y mi nota es: " + this.Nota;
+        }
+        public Estudiante(int pId, string pNombre, int pEdad, double pNota)
+        {            
+            Id = pId;
+            Nombre = pNombre;
+            Edad = pEdad;
+            Nota = pNota;
+        }
+    }
+}
---
+++ AplicacionClase1/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using AplicacionClase1;
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+List<Estudiante> listaEstudiantes = new List<Estudiante>();
+
+int opcion = 0;
+
+while (opcion != -1)
+{
+    Console.WriteLine("Bienvenido, ingrese una opción");
+    Console.WriteLine("1 - Alta de Alumnos");
+    Console.WriteLine("2 - Listado de Alumnos");
+    Console.WriteLine("3 - Promedio de Notas");
+    Console.WriteLine("4 - Estudiantes aprobados");
+
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case -1: Console.WriteLine("Saliendo del sistema..."); break;
+
+        case 1: AltaAlumno(); break;
+
+        case 2: ListarAlumnos(); break;
+
+        case 3: PromediosNotas(); break;
+
+        case 4: MostrarAprobados(); break;
+
+
+        default:
+            Console.WriteLine("Opción incorrecta");
+            break;
+    }
+
+}
+
+void AltaAlumno()
+{
+    Console.WriteLine("Ingrese el Nombre"):
+    string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese la Edad"):
+    int edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la Nota"):
+    int nota = int.Parse(Console.ReadLine());
+
+Estudiante nuevoEstudiante = new Estudiante(nombre, edad, nota);
+
+listaEstudiantes.Add(nuevoEstudiante);
+
+Console.WriteLine("Estudiante agregado");
+}
+
+void ListarAlumnos()
+{
+    foreach(Estudiante e in listaEstudiantes)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
+
+void PromediosNotas()
+{
+    double total = 0;
+
+    foreach (Estudiante e in listaEstudiantes)
+    {
+        total += e.Nota;
+    }   
+
+    Console.WriteLine("El promedio es de: " + total / listaEstudiantes.Count);
+}
+
+void MostrarAprobados()
+{
+    Console.WriteLine("Estudiantes aprobados:");
+
+    foreach (Estudiante e in listaEstudiantes)
+    {
+        if (e.Nota > 60)
+        {
+            Console.WriteLine(e.Nombre);
+        }
+    }
+}
